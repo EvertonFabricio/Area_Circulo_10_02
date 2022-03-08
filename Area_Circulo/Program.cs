4c9b33a9ebad32a133ddda54e08748d75d36ca6b
EvertonFabricio <everton.5by5@gmail.com>
2022-03-08 10:01:44 -0300

    Adicionar arquivos de projeto.

---
+++ Area_Circulo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Area_Circulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a medida, em centimetros, do raio de um circulo qualquer: ");
+            float raio = float.Parse(Console.ReadLine());
+         
+            Console.WriteLine($"\nA area de um circulo de raio igual a {raio} centimetros é {raio*raio*3.14} centimetros quadrados");
+
+
+        }
+    }
+}
